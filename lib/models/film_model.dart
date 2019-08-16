f4c62b51c03a4d408c8babf242464a66ff40b590
Pedro Augusto Paiao L. da Silva <51393746+PedroPaiao@users.noreply.github.com>
2019-08-16 18:06:49 -0400

    PeersOfPeer

--- lib/models/film_model.dart
+++ lib/models/film_model.dart
@@ -5,7 +5,10 @@ import 'package:starwars_revenge/ui/film_page.dart';
 class FilmModel extends StatefulWidget {
   List characters = [];
   List planets = [];
+  List starships = [];
   bool completed = false;
+  List vehicles = [];
+  List species = [];
   Map _item;
 
   String _title;
@@ -52,54 +55,77 @@ class FilmModel extends StatefulWidget {
     return map;
   }
 
-
   @override
   _FilmModelState createState() => _FilmModelState();
 }
 
 class _FilmModelState extends State<FilmModel> {
-  constructingTheBank() async{
+  constructingTheBank() async {
     Response response;
-    for(String link in widget._item['characters']) {
+    for (String link in widget._item['characters']) {
       response = await Dio().get(link);
       widget.characters.add(response.data);
     }
-    for(String link in widget._item['planets']) {
+    for (String link in widget._item['planets']) {
       response = await Dio().get(link);
       widget.planets.add(response.data);
+    }
+    for (String link in widget._item['starships']) {
+      response = await Dio().get(link);
+      widget.starships.add(response.data);
+    }
+    for (String link in widget._item['vehicles']) {
+      response = await Dio().get(link);
+      widget.vehicles.add(response.data);
+    }
+    for (String link in widget._item['species']) {
+      response = await Dio().get(link);
+      widget.species.add(response.data);
     }
     print('entrou');
     setState(() {
       widget.completed = true;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    if(widget.characters.length == 0 && widget.planets.length == 0)constructingTheBank(); //Cancela as repetições
-    return  GestureDetector(
+    if (widget.characters.length == 0 &&
+        widget.planets.length == 0 &&
+        widget.starships.length == 0 &&
+        widget.vehicles.length == 0 &&
+        widget.species.length == 0)
+      constructingTheBank(); //Cancela as repetições
+    return GestureDetector(
       child: Card(
           child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: !widget.completed ?  Center(child: CircularProgressIndicator()) : Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-            Text(widget._item['title'],
-                style: TextStyle(
-                    fontStyle: FontStyle.italic,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w600)),
-            SizedBox(height: 5.0),
-            Text(widget._item['release_date']),
-            SizedBox(height: 5.0),
-            Text(widget._item['director']),
-            SizedBox(height: 5.0),
-            Text(widget._item['opening_crawl']),
-        ],
-      ),
-          )),
+        padding: const EdgeInsets.all(10.0),
+        child: !widget.completed
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(widget._item['title'],
+                      style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w600)),
+                  SizedBox(height: 5.0),
+                  Text(widget._item['release_date']),
+                  SizedBox(height: 5.0),
+                  Text(widget._item['director']),
+                  SizedBox(height: 5.0),
+                  Text(widget._item['opening_crawl']),
+                ],
+              ),
+      )),
       onTap: () {
-        if(widget.completed)Navigator.push(
-            context, MaterialPageRoute(builder: (_) => FilmPage(widget._item, widget.characters, widget.planets)));
+        if (widget.completed)
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (_) => FilmPage(widget._item, widget.characters,
+                      widget.planets, widget.starships, widget.vehicles, widget.species)));
       },
     );
   }
--- lib/ui/film_page.dart
+++ lib/ui/film_page.dart
@@ -4,11 +4,17 @@ class FilmPage extends StatefulWidget {
   Map item;
   List character;
   List planets;
+  List starships;
+  List vehicles;
+  List species;
 
   FilmPage(
     this.item,
     this.character,
     this.planets,
+    this.starships,
+    this.vehicles,
+    this.species,
   );
 
   @override
@@ -30,62 +36,98 @@ class _FilmPageState extends State<FilmPage> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Text('Episodio: '),
-                Text('${widget.item['episode_id']}'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Episodio: ',
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          decoration: TextDecoration.underline)),
+                ),
+                Text('${widget.item['episode_id']}',
+                    style: TextStyle(fontSize: 18.0)),
               ],
             ),
             Row(
               children: <Widget>[
-                Text('Diretor: '),
-                Text('${widget.item['director']}'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Diretor: ', style: TextStyle(fontSize: 11.0)),
+                ),
+                Text('${widget.item['director']}',
+                    style: TextStyle(
+                        fontSize: 16.0, decoration: TextDecoration.underline)),
               ],
             ),
             Row(
               children: <Widget>[
-                Text('Produtor(a): '),
-                Text('${widget.item['producer']}'),
+                Text('Produtor(a): ', style: TextStyle(fontSize: 11.0)),
+                Text('${widget.item['producer']}',
+                    style: TextStyle(
+                        fontSize: 11.0, decoration: TextDecoration.underline)),
               ],
             ),
             Row(
               children: <Widget>[
-                Text('Data de Lançamento: '),
-                Text('${widget.item['release_date']}'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Data de Lançamento: ',
+                      style: TextStyle(
+                          fontSize: 12.0,
+                          decoration: TextDecoration.underline)),
+                ),
+                Text('${widget.item['release_date']}',
+                    style: TextStyle(fontSize: 12.0)),
               ],
             ),
-            Text('Atores participantes: '),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text('Atores participantes: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
+            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: widget.character.map((item) {
                 return Text(item['name']);
               }).toList(),
             ),
-            Text('Planetas participantes: '),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text('Planetas participantes: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
+            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: widget.planets.map((item) {
                 return Text(item['name']);
               }).toList(),
             ),
-            Row(
-              children: <Widget>[
-                //Arrumar
-                Text('Episodio: '),
-                Text('${widget.item['episode_id']}'),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text('Naves estelares: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
             ),
-            Row(
-              children: <Widget>[
-                //Arrumar
-                Text('Episodio: '),
-                Text('${widget.item['episode_id']}'),
-              ],
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: widget.starships.map((item) {
+                return Text(item['name']);
+              }).toList(),
             ),
-            Row(
-              children: <Widget>[
-                //Arrumar
-                Text('Episodio: '),
-                Text('${widget.item['episode_id']}'),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text('Veiculos: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: widget.vehicles.map((item) {
+                return Text(item['name']);
+              }).toList(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text('Espécies: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: widget.species.map((item) {
+                return Text(item['name']);
+              }).toList(),
             ), //Padrao de Texto
           ],
         ),
