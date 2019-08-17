78a58eeb46049cee107a2dc99bab48cf528fe024
Pedro Augusto Paiao L. da Silva <51393746+PedroPaiao@users.noreply.github.com>
2019-08-17 09:48:16 -0400

    TheImagesOfTheBanking

--- lib/ui/film_page.dart
+++ lib/ui/film_page.dart
@@ -31,105 +31,152 @@ class _FilmPageState extends State<FilmPage> {
         title: Text(widget.item['title']),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Episodio: ',
-                      style: TextStyle(
-                          fontSize: 16.0,
-                          decoration: TextDecoration.underline)),
-                ),
-                Text('${widget.item['episode_id']}',
-                    style: TextStyle(fontSize: 18.0)),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Diretor: ', style: TextStyle(fontSize: 11.0)),
-                ),
-                Text('${widget.item['director']}',
-                    style: TextStyle(
-                        fontSize: 16.0, decoration: TextDecoration.underline)),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Text('Produtor(a): ', style: TextStyle(fontSize: 11.0)),
-                Text('${widget.item['producer']}',
-                    style: TextStyle(
-                        fontSize: 11.0, decoration: TextDecoration.underline)),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Data de Lançamento: ',
-                      style: TextStyle(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Episodio: ',
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                            decoration: TextDecoration.underline)),
+                  ),
+                  Text('${widget.item['episode_id']}',
+                      style: TextStyle(fontSize: 18.0)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Diretor: ',
+                        style: TextStyle(
                           fontSize: 12.0,
-                          decoration: TextDecoration.underline)),
-                ),
-                Text('${widget.item['release_date']}',
-                    style: TextStyle(fontSize: 12.0)),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text('Atores participantes: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: widget.character.map((item) {
-                return Text(item['name']);
-              }).toList(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text('Planetas participantes: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: widget.planets.map((item) {
-                return Text(item['name']);
-              }).toList(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text('Naves estelares: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: widget.starships.map((item) {
-                return Text(item['name']);
-              }).toList(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text('Veiculos: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: widget.vehicles.map((item) {
-                return Text(item['name']);
-              }).toList(),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text('Espécies: ', style: TextStyle(decoration: TextDecoration.underline, fontSize: 13)),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: widget.species.map((item) {
-                return Text(item['name']);
-              }).toList(),
-            ), //Padrao de Texto
-          ],
+                          fontWeight: FontWeight.w600,
+                        )),
+                  ),
+                  Text('${widget.item['director']}',
+                      style: TextStyle(
+                          fontSize: 16.0, decoration: TextDecoration.underline)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: Text('Produtor(a): ',
+                        style: TextStyle(
+                          fontSize: 11.0,
+                          fontWeight: FontWeight.w600,
+                        )),
+                  ),
+                  Flexible(
+                    fit: FlexFit.loose,
+                    child: Text('${widget.item['producer']}',
+                        overflow: TextOverflow.clip,
+                        softWrap: true,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 13.0, decoration: TextDecoration.underline)),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Data de Lançamento: ',
+                        style: TextStyle(
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w600,
+                            decoration: TextDecoration.underline)),
+                  ),
+                  Text('${widget.item['release_date']}',
+                      style: TextStyle(fontSize: 12.0)),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text('Atores participantes: ',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    )),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: widget.character.map((item) {
+                  return Text(item['name']);
+                }).toList(),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text('Planetas: ',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    )),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: widget.planets.map((item) {
+                  return Text(item['name']);
+                }).toList(),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text('Naves estelares: ',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    )),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: widget.starships.map((item) {
+                  return Text(item['name']);
+                }).toList(),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text('Veiculos: ',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    )),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: widget.vehicles.map((item) {
+                  return Text(item['name']);
+                }).toList(),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text('Espécies: ',
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        fontSize: 13,
+                        fontWeight: FontWeight.w600)),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: widget.species.map((item) {
+                  return Text(item['name']);
+                }).toList(),
+              ), //Padrao de Texto
+            ],
+          ),
         ),
       ),
     );
