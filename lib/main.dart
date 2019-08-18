851e8fb0d732471b7013b30602814f42c5765419
Pedro Augusto Paiao L. da Silva <51393746+PedroPaiao@users.noreply.github.com>
2019-08-18 10:54:24 -0400

    TheMusicIsNecessery

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:starwars_revenge/ui/home_page.dart';
-
 import 'models/star_wars_model.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  final assets = <String>[
+    "Theme.mp3",
+  ];
+  final AssetsAudioPlayer _assetsAudioPlayer = AssetsAudioPlayer();
+
+  void _playPause() {
+    _assetsAudioPlayer.play();
+  }
+  void _open(int assetIndex) {
+    _assetsAudioPlayer.open(
+      AssetsAudio(
+        asset: assets[assetIndex],
+        folder: "assets/audios/",
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    _open(0);
     return ScopedModel<StarWarsModel>(
         model: StarWarsModel(),
         child: MaterialApp(
