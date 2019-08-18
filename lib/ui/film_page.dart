224820b410272f24da6eaddbd03c810795177266
Pedro Augusto Paiao L. da Silva <51393746+PedroPaiao@users.noreply.github.com>
2019-08-17 22:19:51 -0400

    CloseDoClose

--- lib/ui/film_page.dart
+++ lib/ui/film_page.dart
@@ -22,150 +22,177 @@ class _FilmPageState extends State<FilmPage> {
         centerTitle: true,
         title: Text(widget._item['title']),
       ),
-      body: ScopedModelDescendant<StarWarsModel>(
-        builder: (context, child, model) {
-          int index = model.films.indexOf(widget._item);
-          if (!widget.loaded) {
-            widget.loaded = true;
-            model.chargeEverything(index);
-          }
-          return model.isLoading
-              ? Center(child: CircularProgressIndicator())
-              : SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text('Episodio: ',
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/back.jpg'),
+          fit: BoxFit.cover),
+        ),
+        child: ScopedModelDescendant<StarWarsModel>(
+          builder: (context, child, model) {
+            int index = model.films.indexOf(widget._item);
+            if (!widget.loaded) {
+              widget.loaded = true;
+              model.chargeEverything(index);
+            }
+            return model.isLoading
+                ? Center(child: CircularProgressIndicator())
+                : SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('Episodio: ',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                      decoration: TextDecoration.underline)),
+                            ),
+                            Text('${model.films[index]['episode_id']}',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.white,
+                                )),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('Diretor: ',
+                                  style: TextStyle(
+                                      fontSize: 11.0, color: Colors.white)),
+                            ),
+                            Text('${model.films[index]['director']}',
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontSize: 16.0,
                                     decoration: TextDecoration.underline)),
-                          ),
-                          Text('${model.films[index]['episode_id']}',
-                              style: TextStyle(fontSize: 18.0, color: Colors.white,)),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text('Diretor: ',
-                                style: TextStyle(fontSize: 11.0, color: Colors.white)),
-                          ),
-                          Text('${model.films[index]['director']}',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16.0,
-                                  decoration: TextDecoration.underline)),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Text('Produtor(a): ',
-                              style: TextStyle(fontSize: 11.0,color: Colors.white)),
-                          Text('${model.films[index]['producer']}',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 11.0,
-                                  decoration: TextDecoration.underline)),
-                        ],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text('Data de Lançamento: ',
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('Produtor(a): ',
+                                style: TextStyle(
+                                    fontSize: 11.0, color: Colors.white)),
+                            Text('${model.films[index]['producer']}',
                                 style: TextStyle(
                                     color: Colors.white,
-                                    fontSize: 12.0,
+                                    fontSize: 11.0,
                                     decoration: TextDecoration.underline)),
-                          ),
-                          Text('${model.films[index]['release_date']}',
-                              style: TextStyle(fontSize: 12.0, color: Colors.white)),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text('Atores participantes: ',
-                            style: TextStyle(
-                                color: Colors.white,
-                                decoration: TextDecoration.underline,
-                                fontSize: 13)),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: model.films[index]['characters']
-                            .map<Widget>((item) {
-                          return Text(item['name']);
-                        }).toList(),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text('Planetas participantes: ',
-                            style: TextStyle(
-                                color: Colors.white,
-                                decoration: TextDecoration.underline,
-                                fontSize: 13)),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children:
-                            model.films[index]['planets'].map<Widget>((_item) {
-                          return Text(_item['name']);
-                        }).toList(),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text('Naves estelares: ',
-                            style: TextStyle(
-                                color: Colors.white,
-                                decoration: TextDecoration.underline,
-                                fontSize: 13)),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: model.films[index]['starships']
-                            .map<Widget>((_item) {
-                          return Text(_item['name']);
-                        }).toList(),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text('Veiculos: ',
-                            style: TextStyle(
-                                color: Colors.white,
-                                decoration: TextDecoration.underline,
-                                fontSize: 13)),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children:
-                            model.films[index]['vehicles'].map<Widget>((_item) {
-                          return Text(_item['name']);
-                        }).toList(),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text('Espécies: ',
-                            style: TextStyle(
-                                color: Colors.white,
-                                decoration: TextDecoration.underline,
-                                fontSize: 13)),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children:
-                            model.films[index]['species'].map<Widget>((_item) {
-                          return Text(_item['name']);
-                        }).toList(),
-                      ), //Padrao de Texto
-                    ],
-                  ),
-                );
-        },
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('Data de Lançamento: ',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12.0,
+                                      decoration: TextDecoration.underline)),
+                            ),
+                            Text('${model.films[index]['release_date']}',
+                                style: TextStyle(
+                                    fontSize: 12.0, color: Colors.white)),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text('Atores participantes: ',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 13)),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: model.films[index]['characters']
+                              .map<Widget>((item) {
+                            return Text('${item['name']}',
+                                style: TextStyle(color: Colors.white));
+                          }).toList(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text('Planetas envolvidos: ',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 13)),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: model.films[index]['planets']
+                              .map<Widget>((_item) {
+                            return Text(
+                              _item['name'].toString(),
+                              style: TextStyle(color: Colors.white),
+                            );
+                          }).toList(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text('Naves estelares: ',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 13)),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: model.films[index]['starships']
+                              .map<Widget>((_item) {
+                            return Text(
+                              _item['name'].toString(),
+                              style: TextStyle(color: Colors.white),
+                            );
+                          }).toList(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text('Veiculos: ',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 13)),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: model.films[index]['vehicles']
+                              .map<Widget>((_item) {
+                            return Text(
+                              _item['name'].toString(),
+                              style: TextStyle(color: Colors.white),
+                            );
+                          }).toList(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text('Espécies: ',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 13)),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: model.films[index]['species']
+                              .map<Widget>((_item) {
+                            return Text(_item['name'].toString(),
+                                style: TextStyle(color: Colors.white));
+                          }).toList(),
+                        ), //Padrao de Texto
+                      ],
+                    ),
+                  );
+          },
+        ),
       ),
     );
   }
